e27596ca8db69fb3bdbb01003a5d0a000f38d285
Gzanella1 <giovanizanelladamaia123@gmail.com>
2025-08-01 11:03:26 -0300

    ajkdkjsakfjiajf[]

--- testeGit/src/testeGit/main.java
+++ testeGit/src/testeGit/main.java
@@ -3,5 +3,7 @@ package testeGit;
 public class main {
 	public static void main(String[] args) {
 		System.out.println("giovani o mais lindo ");
+		System.out.println("giovani o mais lindo ");
+
 	}
 }
